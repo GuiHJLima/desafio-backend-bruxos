73b3a6439e714405a2538a98340d742a22c61b82
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2024-04-24 10:57:53 -0300

    feat: tables

--- db/script.sql
+++ db/script.sql
@@ -1,15 +1,35 @@
 -- Criação do banco de dados
-CREATE DATABASE limaaulabacktds2;
+CREATE DATABASE usuariosdolima;
 
 -- Conecta no banco de dados
-\c limaaulabacktds2;
+\c usuariosdolima;
 
--- Criação da tabela 'usuario'
-CREATE TABLE usuario (
+-- Criação da tabela 'wizard'
+CREATE TABLE wizard (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    age INT NOT NULL,
+    house VARCHAR(100) NOT NULL,
+    special_ability VARCHAR(100) NOT NULL,
+    blood_status VARCHAR(100) NOT NULL,
+    patronus VARCHAR(100),
+    alive BOOLEAN NOT NULL DEFAULT TRUE
+
 );
 
--- inserção de dados na tabela 'usuario'
-INSERT INTO usuario (nome, email) VALUES ('Lima', 'lima@gmail.com');
+-- Criando a tabela 'wands'
+CREATE TABLE wands (
+    id SERIAL PRIMARY KEY,
+    mateial VARCHAR(100) NOT NULL,
+    length INT NOT NULL,
+    core VARCHAR(100) NOT NULL,
+    date_of_creation DATE NOT NULL,
+);
+
+-- inserção de dados na tabela 'wizard'
+INSERT INTO wizard (name, surname, date_of_birth, age, house, special_ability, blood_status, patronus) VALUES ('Harry', 'Potter', '1980-07-31', 40, 'Gryffindor', 'Parseltongue', 'Half-blood', 'Stag', TRUE);
+
+-- inserção de dados na tabela 'wands'
+INSERT INTO wands (material, length, core, date_of_creation) VALUES ('Holly', 11, 'Phoenix feather', '1991-07-31');
