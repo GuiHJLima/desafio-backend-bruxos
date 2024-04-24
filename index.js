b48eea33e3301a075dacec3da567c2127e8d2789
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2024-04-24 11:56:31 -0300

    feat: edit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function validarSangue(sangue) {
     }
     return true;
 }
-//criar uma rota que obtem todos os wizards
+//rota que obtem todos os wizards
 app.get('/wizard', async (req, res) => {
     try {
         const resultado = await pool.query('SELECT * FROM wizard');
@@ -53,7 +53,7 @@ app.get('/wizard', async (req, res) => {
     }
 });
 
-//criar uma rota que obtem todos os wands
+//rota que obtem todos os wands
 app.get('/wands', async (req, res) => {
     try {
         const resultado = await pool.query('SELECT * FROM wands');
@@ -67,7 +67,7 @@ app.get('/wands', async (req, res) => {
     }
 });
 
-//criar uma rota que obtem um wizard pelo id
+//rota que obtem um wizard pelo id
 app.get('/wizard/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -79,7 +79,7 @@ app.get('/wizard/:id', async (req, res) => {
     }
 });
 
-//criar uma rota que obtem um wand pelo id
+//rota que obtem um wand pelo id
 app.get('/wands/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -91,7 +91,7 @@ app.get('/wands/:id', async (req, res) => {
     }
 });
 
-//criar uma rota que adcione um novo wizard
+//rota que adcione um novo wizard
 app.post('/wizard', async (req, res) => {
     try {
         const { name, surname, date_of_birth, gender, house, special_ability, blood_status, patronus, alive } = req.body;
@@ -114,11 +114,11 @@ app.post('/wizard', async (req, res) => {
     }
 });
 
-//criar uma rota que adcione um novo wand
+//rota que adcione um novo wand
 app.post('/wands', async (req, res) => {
     try {
-        const { wood, core, length, flexibility } = req.body;
-        await pool.query('INSERT INTO wands (wood, core, length, flexibility) VALUES ($1, $2, $3, $4)', [wood, core, length, flexibility]);
+        const { material, core, length, date_of_creation } = req.body;
+        await pool.query('INSERT INTO wands (material, core, length, date_of_creation) VALUES ($1, $2, $3, $4)', [material, core, length, date_of_creation]);
         res.send({ mensagem: "Wand adicionado com sucesso" });
     } catch (error) {
         console.error("Erro ao tentar adicionar um wand", error);
@@ -126,28 +126,62 @@ app.post('/wands', async (req, res) => {
     }
 });
 
-//criar uma rota que delete um usuario]
-app.delete('/usuario/:id', async (req, res) => {
+//rota que delete um wizard
+app.delete('/wizard/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        await pool.query('DELETE FROM usuario WHERE id = $1', [id]);
-        res.send({ mensagem: "Usuario deletado com sucesso" });
+        await pool.query('DELETE FROM wizard WHERE id = $1', [id]);
+        res.send({ mensagem: "Wizard deletado com sucesso" });
     } catch (error) {
-        console.error("Erro ao tentar deletar um usuario");
-        res.status(500).send({ mensagem: "Erro ao tentar deletar um usuario" });
+        console.error("Erro ao tentar deletar um wizard");
+        res.status(500).send({ mensagem: "Erro ao tentar deletar um wizard" });
     }
 });
 
-//criar uma rota que atualize um usuario
-app.put('/usuario/:id', async (req, res) => {
+//rota que delete um wand
+app.delete('/wands/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const { nome, email } = req.body;
-        await pool.query('UPDATE usuario SET nome = $1, email = $2 WHERE id = $3', [nome, email, id]);
-        res.send({ mensagem: "Usuario atualizado com sucesso" });
+        await pool.query('DELETE FROM wands WHERE id = $1', [id]);
+        res.send({ mensagem: "Wand deletado com sucesso" });
     } catch (error) {
-        console.error("Erro ao tentar atualizar um usuario");
-        res.status(500).send({ mensagem: "Erro ao tentar atualizar um usuario" });
+        console.error("Erro ao tentar deletar um wand");
+        res.status(500).send({ mensagem: "Erro ao tentar deletar um wand" });
+    }
+});
+
+//rota que atualize um wizard
+app.put('/wizard/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, surname, date_of_birth, gender, house, special_ability, blood_status, patronus, alive } = req.body;
+        const idade = calcularIdade(new Date(date_of_birth));
+        if (!validarCasa(house)) {
+            res.status(400).send({ mensagem: "Casa invalida, por favor insira uma dessas casas: Gryffindor, Hufflepuff, Ravenclaw, Slytherin" });
+            return;
+        }
+        if (!validarSangue(blood_status)) {
+            res.status(400).send({ mensagem: "Sangue invalido, por favor insira um desses sangues: Muggle, Half-blood, Pure-blood" });
+            return;
+        }
+        await pool.query('UPDATE wizard SET name = $1, surname = $2, date_of_birth = $3, age = $4, gender = $5, house = $6, special_ability = $7, blood_status = $8, patronus = $9, alive = $10 WHERE id = $11', [name, surname, date_of_birth, idade, gender, house, special_ability, blood_status, patronus, alive, id]);
+        res.send({ mensagem: "Wizard atualizado com sucesso" });
+    } catch (error) {
+        console.error("Erro ao tentar atualizar um wizard");
+        res.status(500).send({ mensagem: "Erro ao tentar atualizar um wizard" });
+    }
+} );
+
+//rota que atualize um wand
+app.put('/wands/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { material, core, length, date_of_creation } = req.body;
+        await pool.query('UPDATE wands SET material = $1, core = $2, length = $3, date_of_creation = $4 WHERE id = $5', [material, core, length, date_of_creation, id]);
+        res.send({ mensagem: "Wand atualizado com sucesso" });
+    } catch (error) {
+        console.error("Erro ao tentar atualizar um wand");
+        res.status(500).send({ mensagem: "Erro ao tentar atualizar um wand" });
     }
 });
 
